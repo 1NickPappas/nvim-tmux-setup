ce380e364c91f50a85cff2c770fd056e96e09d2c
Nick Pappas <nick.pappas.ee@gmail.com>
2024-06-10 13:39:48 +0300

    vim-tmux navigation for nvim and tmux. Works great

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,4 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
---
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,7 @@
+return {
+  'christoomey/vim-tmux-navigator',
+  vim.keymap.set('n','C-h',':TmuxNavigateLeft<CR>'),
+  vim.keymap.set('n','C-h',':TmuxNavigateLeft<CR>'),
+  vim.keymap.set('n','C-h',':TmuxNavigateLeft<CR>'),
+  vim.keymap.set('n','C-h',':TmuxNavigateLeft<CR>')
+}
