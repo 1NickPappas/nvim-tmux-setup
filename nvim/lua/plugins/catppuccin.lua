6721fba096d61addf068ca0e8aae86629a728f04
Nick Pappas <nick.pappas.ee@gmail.com>
2024-06-11 02:57:37 +0300

    some changes

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,5 +1,27 @@
 return { "catppuccin/nvim", name = "catppuccin", priority = 1000,
   config = function()
-    vim.cmd.colorscheme "catppuccin"
+    require("catppuccin").setup({
+				integrations = {
+					cmp = true,
+					fidget = true,
+					gitsigns = true,
+					harpoon = true,
+					indent_blankline = {
+						enabled = false,
+						scope_color = "sapphire",
+						colored_indent_levels = false,
+					},
+					mason = true,
+					native_lsp = { enabled = true },
+					noice = true,
+					notify = true,
+					symbols_outline = true,
+					telescope = true,
+					treesitter = true,
+					treesitter_context = true,
+				},
+			})
+
+			vim.cmd.colorscheme("catppuccin-macchiato")
   end
 }
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -10,3 +10,35 @@ vim.keymap.set('n','<c-k>',':wincmd k<CR>')
 vim.keymap.set('n','<c-j>',':wincmd j<CR>')
 vim.keymap.set('n','<c-h>',':wincmd h<CR>')
 vim.keymap.set('n','<c-l>',':wincmd l<CR>')
+
+
+-- Enable relative line numbers
+vim.opt.nu = true
+vim.opt.rnu = true
+
+-- Set tabs to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+-- Enable mouse mode
+vim.opt.mouse = "a"
+
+-- Enable the sign column to prevent the screen from jumping
+vim.opt.signcolumn = "yes"
+
+-- Enable cursor line highlight
+vim.opt.cursorline = true
+
+-- Place a column line
+vim.opt.colorcolumn = "80"
+
+--Need to play aroubd with this for better cursor
+vim.opt.guicursor = {
+	"n-v-c:block", -- Normal, visual, command-line: block cursor
+	"i-ci-ve:ver25", -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
+	"r-cr:hor20", -- Replace, command-line replace: horizontal bar cursor with 20% height
+	"o:hor50", -- Operator-pending: horizontal bar cursor with 50% height
+	"a:blinkwait700-blinkoff400-blinkon250", -- All modes: blinking settings
+	"sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
+}
